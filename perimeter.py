ab946cf2b58ff53e1d0790ceb8247babb54295fb
PravallikaSomisetti <163846578+PravallikaSomisetti@users.noreply.github.com>
2024-10-08 11:59:02 +0530

    Created using Colab

---
+++ perimeter.py
@@ -0,0 +1,58 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPwSkOpBSYErQysvjIxIFSv",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/PravallikaSomisetti/FMML-LABS/blob/main/perimeter.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "SI3B9d0xxOjO",
+        "outputId": "83e81c9a-4400-468e-8cb8-03373cd90a56"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "enter length: 5\n",
+            "enter width: 8\n",
+            "26\n"
+          ]
+        }
+      ],
+      "source": [
+        "l=int(input(\"enter length: \"))\n",
+        "w=int(input(\"enter width: \"))\n",
+        "perimeter=2*(l+w)\n",
+        "print(perimeter)"
+      ]
+    }
+  ]
+}
